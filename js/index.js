af87403829f712d15adcdffc15cfdd804fc8f770
TazorDE <malteteichert@web.de>
2020-07-10 13:22:37 +0200

    add Time validaton, begin fetchign/posting data

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let user = "";
+let url;
 let eventName, eventLocation, orgMail, startDate, startTime, endDate, endTime;
 
 function submitUserID(e) {
@@ -11,6 +12,13 @@ function submitUserID(e) {
     document.getElementById('mainContent').style.display = 'grid';
     //build the URL to the Webservice
     buildURL();
+    //test getting the JSON
+    fetch(url)
+        .then(res => res.json())
+        .then((out) => {
+            console.log(out);
+        })
+        .catch(err => { throw err });
 }
 
 function buildURL() {
@@ -24,6 +32,7 @@ function buildURL() {
 function switchCreateEntry() {
     document.getElementById('mainContent').style.display = 'none';
     document.getElementById('createEvent').style.display = 'grid';
+    document.getElementById('timeWarning').style.display = 'none';
 }
 
 function newEvent(e) {
@@ -36,45 +45,119 @@ function newEvent(e) {
     console.log(eventName, eventLocation, orgMail,);
     document.getElementById('setEventTime').style.display = 'grid';
     document.getElementById('createEvent').style.display = 'none';
+    document.getElementById('timeWarning').style.display = 'none';
     return true;
 }
 
 function setTime(e) {
     e.preventDefault();
-    console.log("setTime");
+    console.log("setTime " + document.getElementById('starttime').value);
     startDate = document.getElementById('startdate').value;
     startTime = document.getElementById('starttime').value;
     endDate = document.getElementById('enddate').value;
     endTime = document.getElementById('endtime').value;
-    validateTime();
-    console.log(startDate, startTime, endDate, endTime);
-    
-    document.getElementById('mainContent').style.display = 'grid';
-    document.getElementById('setEventTime').style.display = 'none';
+    if (validateDate()) {
+
+        document.getElementById('mainContent').style.display = 'grid';
+        document.getElementById('setEventTime').style.display = 'none';
+        document.getElementById('timeWarning').style.display = 'none';
+
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function validateTime(){
-    let startHr = parseInt(startTime.substr(0,2)); //validated through html pattern
-    let startMin = parseInt(startTime.substr(3,2));
-    let endHr = parseInt(endTime.substr(0,2));
-    let endMin = parseInt(endTime.substr(3,2));
+function validateTime() {
+    let startHr = parseInt(startTime.substr(0, 2)); //validated through html pattern
+    let startMin = parseInt(startTime.substr(3, 2));
+    let endHr = parseInt(endTime.substr(0, 2));
+    let endMin = parseInt(endTime.substr(3, 2));
 
-    if(startDate == endDate){
-        if(startTime == endTime){
-            console.log("start time == end time");
-        }else if(startHr > endHr){
-            console.error("start hr is after end hr");
-        }else if(startHr < endHr){
-            console.log("start hr is before end hr");
-        }else if(startHr == endHr){
-            console.log("start hr is equal to end hr");
-            if(startMin > endMin){
-                console.error("start min is after end min");
-            }else if(startMin < endMin){
-                console.log("start min is before end min");
-            }else{
-                console.log("start min is equal to end min");
-            }
+    if (startTime == endTime) {
+        console.log("start time is equal to end time");
+        return true;
+    } else if (startHr > endHr) {
+        console.error("start hr is after end hr");
+        return false;
+    } else if (startHr < endHr) {
+        console.log("start hr is before end hr");
+        return true;
+    } else if (startHr == endHr) {
+        console.log("start hr is equal to end hr");
+        if (startMin > endMin) {
+            console.error("start min is after end min");
+            return false;
+        } else if (startMin < endMin) {
+            console.log("start min is before end min");
+            return true;
+        } else {
+            console.log("start min is equal to end min");
+            return true;
         }
     }
+}
+
+function validateDate() {
+    let startYear = parseInt(startDate.substr(0, 4), 10);
+    let startMonth = parseInt(startDate.substr(5, 2), 10);
+    let startDay = parseInt(startDate.substr(8, 2), 10);
+    let endYear = parseInt(endDate.substr(0, 4), 10);
+    let endMonth = parseInt(endDate.substr(5, 2), 10);
+    let endDay = parseInt(endDate.substr(8, 2), 10);
+
+    console.log(startDate);
+    console.log(startYear + ',' + startMonth + ',' + startDay);
+    console.log(endYear + ',' + endMonth + ',' + endDay);
+
+    if (startYear == endYear) {
+        if (startMonth == endMonth) {
+            if (startDay == endDay) {
+                if (validateTime()) {
+                    return true;
+                } else {
+                    document.getElementById('timeWarning').style.display = 'grid';
+                    return false;
+                }
+            } else if (startDay > endDay) {
+                document.getElementById('timeWarning').style.display = 'grid';
+                return false;
+            } else {
+                return true;
+            }
+        } else if (startMonth > endMonth) {
+            document.getElementById('timeWarning').style.display = 'grid';
+            return false;
+        } else {
+            return true;
+        }
+    } else if (startYear > endYear) {
+        document.getElementById('timeWarning').style.display = 'grid';
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function buildAndSubmitJSON(){
+    let startT = startDate+'T'+startTime;
+    let endT = endDate+'T'+endTime;
+    let sending = {
+        "title": eventName,
+        "location": eventLocation,
+        "organizer": orgMail,
+        "start": startT,
+        "end": endT,
+        "status": "Busy",
+        "allday": false,
+        "webpage": " http://www.radicalsimplicity.com/",
+        "imagedata": "data:ContentType;base64,ImageContent",
+        "categories": [
+            {
+                "id": 3
+            }
+        ],
+        "extra": null
+    }
+    
 }
